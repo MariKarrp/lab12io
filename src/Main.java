d26166bd2057ec4f97f3939cef1119aa1b0526a8
Maria <karpovamasha2006@gmail.com>
2024-11-12 13:55:14 +0300

    njj

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         try(FileInputStream in = new FileInputStream("src/input.txt");
-            FileOutputStream out = new FileOutputStream("src/output.txt"));
+            FileOutputStream out = new FileOutputStream("src/output.txt"))
+        {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            out.write(buffer, 0, buffer.length);
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 }
